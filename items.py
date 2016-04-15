f149867c9b6ac769c452e3b7dbc92835eb29c477
Pete Lambro <peterlambro@gmail.com>
2016-04-14 22:46:56 -0400

    added getSubmitter function

--- items.py
+++ items.py
@@ -1,5 +1,9 @@
 '''
 items and related functions
+define global variables:
+portalIp = your portal ip address and port, no trailing slash
+portalAuth = ('user','password')
+portalHeaders = {'Content-type':'application/xml'} xml or json
 '''
 import requests
 import xml.etree.ElementTree as ET
@@ -21,3 +25,19 @@ class VSItem(object):
 		for field in timespan.findall('itemdoc:field',ns):
 			if field.find('itemdoc:name',ns).text == portalField:
 				return field.find('itemdoc:value',ns).text
+
+	def getSubmitter(self,portalIp,portalAuth,portalHeaders):
+		submitterQuery = requests.get(portalIp + '/API/item/' + self.itemId + '/metadata/changes', auth=portalAuth, headers=portalHeaders)
+		submitterQuery.encoding = 'utf-8'
+		tree = ET.fromstring(submitterQuery.text)
+		fields = tree.iter('{http://xml.vidispine.com/schema/vidispine}field')
+		submitterFound = False
+		while submitterFound == False:
+			try:
+				field = fields.next()
+			except StopIteration:
+				return 'postingest' #or other default user
+				submitterFound = True
+			if field.attrib['user'] != 'system' and field.attrib['user'] != 'admin':
+				return field.attrib['user']
+				submitterFound = True
