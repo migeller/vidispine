88963bb984cd4dba3babd526c0894169805d6009
Pete Lambro <peterlambro@gmail.com>
2016-04-14 21:15:53 -0400

    added metadata call to item init

--- items.py
+++ items.py
@@ -4,20 +4,17 @@ items and related functions
 import requests
 import xml.etree.ElementTree as ET
 
-class VSItem:
-	def __init__(self, itemId):
+class VSItem(object):
+	def __init__(self, itemId, portalIp, portalAuth):
 		self.itemId = itemId
-
-	def getMetadata(self, portalIp, portalAuth):
-		r = requests.get(portalIp+'/API/item/'+self.itemId+'/?content=metadata',auth=portalAuth)
-		return r
+		self.metadata  = requests.get(portalIp+'/API/item/'+self.itemId+'/?content=metadata',auth=portalAuth)
 
 	def getShapedata(self, portalIp, portalAuth):
 		r = requests.get(portalIp+'/API/item/'+self.itemId+'/?content=shape',auth=portalAuth)
 		return r
 
-	def valueFind(self,xml,portalField):
-		mdTree = ET.fromstring(xml.text)
+	def valueFind(self,portalField):
+		mdTree = ET.fromstring(self.metadata.text)
 		ns = {'itemdoc':'http://xml.vidispine.com/schema/vidispine'}
 		mdRaw = mdTree.find('itemdoc:metadata',ns)
 		timespan = mdRaw.find('itemdoc:timespan',ns)
